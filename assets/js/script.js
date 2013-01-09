13b8d23217f4d6eee9231280011448b77cfb6f40
Byron Luther Lagrone <byronic.entertainment@gmail.com>
2013-01-09 14:01:59 -0600

    Drawing an image
    Note that for this commit to work on a local clone, you'll need a
    testcard.jpg image in your img/ folder.

--- assets/js/script.js
+++ assets/js/script.js
@@ -34,4 +34,33 @@ $(function(){
 		// the data is just the other user's session ID
 		alert('Client with ID ' + data + ' disconnected! :(');
 	});
+
+	// testing drawing a card in hand, and on the field.
+	// also tests clearing the screen.
+	// clicking should toggle back and forth
+	var inHand = true;
+	var testImage = new Image();
+	testImage.src = 'assets/img/testcard.jpg';
+	testImage.onload = function() { ctx.drawImage(testImage, 500, 500);  };
+	alert("Testing " + canvas.height + canvas.width);
+	canvas.mousedown(function(e)
+	{
+		e.preventDefault();
+		// clear canvas for drawing
+		
+		ctx.clearRect(0, 0, 1900, 1000);
+		if(inHand)
+		{
+			//draw on the field, no scaling	
+			ctx.drawImage(testImage, 50, 50)
+			inHand = false;	
+			alert("eh, steve!");		
+		}
+		else
+		{
+			//draw in the hand, scale to 100px wide and 200 px tall
+			ctx.drawImage(testImage, 50, 20, 100, 200);
+			inHand = true;
+		}
+	});
 });
